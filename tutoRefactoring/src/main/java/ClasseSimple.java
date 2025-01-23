b4c6683337e7089c2d9a48025026ae344e114166
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-01-23 10:20:38 +0100

    ⚙️ 1er exercice de refactoring.

---
+++ tutoRefactoring/src/main/java/ClasseSimple.java
@@ -0,0 +1,23 @@
+public class ClasseSimple {
+
+    public ClasseSimple() {
+    }
+
+    /**
+     * Additionne a et b
+     * @param a - premier paramètre
+     * @param b - second paramètre
+     * @return
+     */
+    public int add(int a, int b) {
+        return a + b ;
+    }
+
+    public static void main(String[] args) {
+        new ClasseSimple().add(1,2);
+
+        System.out.println("Hello world");
+    }
+}
+
+
---
+++ tutoRefactoring/src/main/java/DateUtils.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+
+public class DateUtils {
+    public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
+        LocalDate now = LocalDate.now();
+        return now.isAfter(startingDate)
+                &&
+                now.isBefore(endingDate);
+    }
+
+    public static boolean isDateBetween(LocalDate startingDate, LocalDate endingDate, LocalDate now, boolean inclusive) {
+        LocalDate startingDay;
+        LocalDate endingDay;
+
+        if (inclusive) {
+            startingDay = startingDate.minusDays(1);
+            endingDay = endingDate.plusDays(1);
+        } else {
+            startingDay = startingDate;
+            endingDay = endingDate;
+        }
+
+        return now.isAfter(startingDay)
+                &&
+                now.isBefore(endingDay);
+    }
+
+    public static boolean isDateOutside(LocalDate date, LocalDate startingDate, LocalDate endingDate) {
+        return isDateBetween(date, startingDate, endingDate, true);
+    }
+}
